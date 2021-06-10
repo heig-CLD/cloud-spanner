61ec2af2f13ba217ea06a7e7dc68bd5d1a05e834
matt989253 <matthieu.burguburu@gmail.com>
2021-06-10 17:19:35 +0200

    Separate config scripts for env and database
    Co-authored-by: Alexandre Piveteau <alexandre@piveteau.email>

---
+++ config_env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# https://cloud.google.com/spanner/docs/emulator#docker
+gcloud config configurations create emulator
+gcloud config set auth/disable_credentials true
+gcloud config set project noted-episode-316407
+gcloud config set api_endpoint_overrides/spanner http://localhost:9020/
+
+# Creating an instance
+gcloud spanner instances create test-instance --config=emulator --description="Test instance" --nodes=1
+gcloud spanner databases create test-database --instance test-instance
