7c9f4e611b7f076a1dc30e9055ed5adca78dab11
Guy-Laurent Subri <guy-laurent@subri.ch>
2021-06-16 15:48:58 +0200

    Add transaction header

--- client/transactions.go
+++ client/transactions.go
@@ -1,6 +1,11 @@
 package client
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/charmbracelet/lipgloss"
+)
 
 type transaction struct {
 	from      string
@@ -16,5 +21,44 @@ type Transactions struct {
 }
 
 func (transactions Transactions) View() string {
-	return ""
+	rowStyle := lipgloss.NewStyle().
+		Margin(0, 1).
+		Padding(0, 1).
+		Border(lipgloss.RoundedBorder())
+
+	rowString := lipgloss.JoinHorizontal(
+		0,
+		statHeader("(strong)", 150),
+		statHeader("(stale)", 125),
+	)
+
+	return rowStyle.Render(rowString)
+}
+
+func statHeader(transactionType string, amount int64) string {
+	boldStyle := lipgloss.NewStyle().Bold(true)
+	commentStyle := lipgloss.NewStyle().Italic(true)
+
+	titleStyle := lipgloss.NewStyle().
+		Width(15).
+		MarginRight(3).
+		Align(lipgloss.Center)
+
+	titleString := lipgloss.JoinVertical(
+		0,
+		boldStyle.Render("# transactions"),
+		commentStyle.Render(transactionType),
+	)
+
+	content := lipgloss.NewStyle().
+		Render(fmt.Sprintf("%d", amount))
+
+	blockStyle := lipgloss.NewStyle().MarginLeft(3).MarginRight(3)
+	blockString := lipgloss.JoinHorizontal(
+		0,
+		titleStyle.Render(titleString),
+		content,
+	)
+
+	return blockStyle.Render(blockString)
 }
